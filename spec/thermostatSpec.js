dacbb4c004ad56f75bf95ef0003aeb9084395d12
eunkenlow <eunkenlow@gmail.com>
2016-10-26 14:37:44 +0100

    added minimum temperature and power saving mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -19,4 +19,25 @@ describe('Thermostat', function(){
     expect(thermostat.temperature).toEqual(19);
   });
 
+  it('minimum temperature is 10 degrees', function(){
+    for (var i = 1; i <= 20; i++){
+      thermostat.down();
+    }
+    expect(thermostat.temperature).toEqual(10);
+  });
+
+  it('has a max temperature of 25 degrees when power saving mode is on', function(){
+    for (var i = 1; i <= 20; i++){
+      thermostat.up();
+    }
+    expect(thermostat.temperature).toEqual(25);
+  });
+
+  it('has a max temperature of 32 degrees when power saving mode is off', function(){
+    thermostat.powerSavingOff();
+    for (var i = 1; i <= 20; i++){
+      thermostat.up();
+    }
+    expect(thermostat.temperature).toEqual(32);
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,35 @@
+var MINIMUM_TEMPERATURE = 10;
+
 function Thermostat() {
   this.temperature = 20;
+  this.powerSavingMode = true;
+  this.maxTemperature = 25;
 }
 
 Thermostat.prototype.up = function() {
-  this.temperature ++;
+  if (this.temperature >= this.maxTemperature){
+    return;
+  }
+  else{
+    this.temperature ++;
+  }
 };
 
 Thermostat.prototype.down = function() {
+  if (this.temperature <= 10){
+    return;
+  }
+  else{
   this.temperature --;
+  }
+};
+
+Thermostat.prototype.powerSavingOn = function() {
+  this.powerSavingMode = true;
+  this.maxTemperature = 25;
+};
+
+Thermostat.prototype.powerSavingOff = function() {
+  this.powerSavingMode = false;
+  this.maxTemperature = 32;
 };
