8529e347b636dcfb9830f3f057ce2b2af7644ac0
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-10-26 14:25:36 +0100

    Add up and down button

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,4 +8,15 @@ describe('Thermostat', function(){
   it('default temperature of 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  it('increases the temperature with up button', function(){
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  it('decreases the temperature with down button', function(){
+    thermostat.down();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
 });
